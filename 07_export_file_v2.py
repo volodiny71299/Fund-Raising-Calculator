52cf07ea0c4258ff2afb7e8c829083106bf5c7a4
Yara <58008773+volodiny71299@users.noreply.github.com>
2021-05-17 10:54:21 +1200

    07_exportfile

--- 07_export_file_v2.py
+++ 07_export_file_v2.py
@@ -16,25 +16,28 @@ fixed_dict = {
 variable_frame = pandas.DataFrame(variable_dict)
 fixed_frame = pandas.DataFrame(fixed_dict)
 
+# change frames to strings
+variable_txt = pandas.DataFrame.to_string(variable_frame)
+fixed_txt = pandas.DataFrame.to_string(fixed_frame)
+
 product_name = "Custom Mugs"
 profit_target = "$100.00"
 required_sales = "$200.00"
-recommended_price = "$5.00"
+recommended_price = "The recommended price is $5.00"
 
 print(variable_frame)
 
-# change dataframe to string (so it can written to a txt file)
-variable_txt = pandas.DataFrame.to_string(variable_frame)
+to_write = [product_name, variable_txt, fixed_txt, profit_target, required_sales, recommended_price]
 
 # write to file...
-# create file to hold data (add .txt extention
+# create file to hold data (add .txt extension
 file_name = "{}.txt".format(product_name)
 text_file = open(file_name, "w+")
 
 # heading
-text_file.write("*** Fund Raising - {} ***\n\n".format(product_name))
-
-text_file.write(variable_txt)
+for item in to_write:
+    text_file.write(item)
+    text_file.write("\n\n")
 
 # close file
 text_file.close()
